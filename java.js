627a3845b45bb7898e5fa6029a43e3c30731faf6
fabiogabrielli <fabiomgabrielli@gmail.com>
2023-11-01 15:50:26 -0300

    exercicio aula06

---
+++ java.js
@@ -0,0 +1,33 @@
+
+
+function calcularEmprestimo() {
+    var valorEmprestimo = parseFloat(document.getElementById("valorEmprestimo").value);
+    var numeroParcelas = parseInt(document.getElementById("numeroParcelas").value);
+
+    var valorParcela = valorEmprestimo / numeroParcelas;
+        var resultado = document.getElementById("resultado");
+        resultado.innerHTML = ""; // Limpa o conteúdo anterior
+
+        for (let i = 1; i <= numeroParcelas; i++) {
+            resultado.innerHTML += "Parcela " + i + ": R$ " + valorParcela.toFixed(2) + "<br>";
+        }
+
+
+    var modal = document.getElementById("modalEmprestimo");
+    modal.style.display = "block";
+
+
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function() {
+        var modal = document.getElementById("modalEmprestimo");
+        modal.style.display = "none";
+    }
+
+
+    window.onclick = function(event) {
+        var modal = document.getElementById("modalEmprestimo");
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
